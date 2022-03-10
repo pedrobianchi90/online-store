8f9ca1428ba2cfff710ac6ec1e58c052f1b9c1ab
Pedro Bianchi <pedroedm@gmail.com>
2022-03-10 18:50:28 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 import './App.css';
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/ShoppingCart">
+            <ShoppingCart />
           </Route>
         </Switch>
       </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <input type="text" />
+        <Link to="/shoppingcart" data-testid="shopping-cart-button">
+          <button type="button">
+            Carrinho de Compras
+          </button>
+        </Link>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
